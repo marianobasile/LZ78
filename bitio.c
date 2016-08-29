e7d626fb5377affe47ef439daf2753a4cd9ce3d7
Mariano Basile <basilemariano92@gmail.com>
2016-08-29 12:37:51 +0200

    add files to repo

---
+++ bitio.c
@@ -0,0 +1,176 @@
+#include "bitio.h"
+#include <inttypes.h>
+
+struct bit_io
+{
+	FILE *f;
+	uint64_t data;
+	uint wp;
+	uint rp;
+	uint mode;
+};
+
+struct bit_io * bitio_open(const char* filename, uint mode) 
+{	
+/* filename: name of the file to open, mode: 0 READ, 1 WRITE, return: bit_io object dynamically allocated */
+	struct bit_io *b;
+
+	if(filename == NULL || filename[0]=='\0' || mode > 1) 
+	{
+		errno = EINVAL;
+		return NULL;
+	}
+
+	b = (struct bit_io *)calloc(1,sizeof(struct bit_io));
+
+	if(b == NULL) 
+	{
+		errno = ENOMEM;
+		return NULL; 
+	}
+
+	b->f = fopen(filename,(mode == 0?"r":"w"));
+
+	if(b->f == NULL) 
+	{
+		free(b);
+		return NULL;
+	}
+
+	b->mode = mode;
+	/* wp,rp already initialized */
+	return b;
+}
+
+int bitio_close (struct bit_io* b) 
+{/*return: 0 SUCCESS, -1 FAILURE*/
+
+	/*return value of the function */
+	int ret = 0; 
+	if(b == NULL) 
+	{
+		errno = EINVAL;
+		ret = -1;
+		return ret;
+	}
+
+	if(b->mode == 1 && b->wp != 0) 
+	{
+		if(fwrite(&b->data,1,((b->wp)+7)/8,b->f) != (b->wp+7)/8 )
+			ret = -1;
+			
+	}	
+	
+	fclose(b->f);
+	bzero(b,sizeof(*b));
+	free(b);
+	return ret;
+}
+
+int bitio_write(struct bit_io* b, uint size, uint64_t data) 
+{	
+	/* available space in the buffer*/
+	uint space; 
+	if(b == NULL || b->mode != 1 || size > 64) 
+	{
+		errno = EINVAL;
+		return -1;	
+	}
+
+	if (size == 0)
+		return 0;
+
+	space = 64 - b->wp;
+
+	/*clear the higher part of the variable data */
+	data &= ((1UL << size) -1); 
+
+	/*shift data of wp position & copy into b->data */
+	b->data |= data << b->wp; 
+
+	/* there's enough space for the new block in the buffer */
+	if(size <=  space) 
+	{
+		b->wp += size;
+	} 
+	else 
+	{	
+		//printf("%"PRIu64"\n",b->data);	
+		if(fwrite(&(b->data),8,1,b->f) <= 0 ) 
+		{	
+			errno = ENOSPC;
+			return -1;	
+		}	
+		/*fill the buffer b->data with the the bits which did not fit the first time */
+		b->data = data >> space;	
+		b->wp = size - space;		
+	}
+
+	return 0;
+}
+
+int bitio_read(struct bit_io* b, uint max_size, uint64_t * result) 
+{
+	uint space;
+	int ret;
+	if(b == NULL || b->mode != 0 || max_size > 64) 
+	{
+		errno = EINVAL;
+		return -1;	
+	}
+
+	*result = 0;
+
+	space = b->wp - b->rp;
+
+	if(max_size == 0)
+		return 0;
+
+	if(max_size <= space) 
+	{	
+		/*first throw away the already read part, then put to 0 the bits after max_size */
+		*result = (b->data >> b->rp) & ((1UL << max_size)-1); 
+		b->rp += max_size;
+		return max_size;
+	}
+	else
+	{
+		*result = (b->data >> b->rp);
+		ret = fread(&b->data,1,8,b->f);
+		if(ret <= 0) 
+		{
+			errno = ENODATA;
+			return -1;
+		}
+		/* b->wp = # of elements successfully read * 8 since each element is 8 bits long */
+		b->wp = ret * 8; 
+
+		/* shift b->data to left by space position (the quantity already used to fill it) & copy in result */
+	
+
+		/* At this point we need to copy the remaining chunk of the data (max_size - space) */
+		/* We may have read from the file less than (max_size - space) bits */
+		if(b->wp >= max_size - space) 
+		{	printf("\nLeggo result: %"PRIu64, *result);
+			*result^=*result;
+			printf("\ndopo result: %"PRIu64, *result);
+
+			printf("\ndopo 2 result: %"PRIu64, b->data);
+			*result |= b->data << space;
+			printf("\ndopo 2 result after: %"PRIu64, *result);
+			/*need to clear extra bits copied in the previous operation (max_size)*/
+			*result &= ((1UL << max_size) - 1); 
+			printf("\ndopo 3 result: %"PRIu64, *result);
+			b->rp = max_size - space;
+			return max_size;
+		}
+
+		*result |= b->data << space;
+		/*need to clear extra bits copied in the previous operation (space + b->wp)*/
+		*result &= ((1UL << (b->wp + space)) - 1); 
+		b->rp = b->wp; 
+		return b->wp + space;	
+
+			
+	}
+}
---
+++ lz78.c
@@ -0,0 +1,527 @@
+#include "lz78.h"
+#include <inttypes.h>
+
+struct hash_entry {
+	uint16_t parent;			//Index of the parent node
+	uint16_t index;				//Node index
+	struct hash_entry* next;	//List of entries (because collisions)
+};
+
+struct hash_entry *hash_table[DICTIONARY_SIZE];
+
+const uint16_t ROOT_ID = 0;
+uint16_t parent_node;
+uint16_t previous_parent_node;
+uint16_t node_id;
+
+/*the global counter*/
+uint16_t counter;
+
+void hash_init() 
+{
+	int i;
+
+	for(i=0; i < DICTIONARY_SIZE; i++)
+	{
+		hash_table[i] -> index = 0;
+		hash_table[i] -> next = NULL;
+	}
+
+}
+
+uint16_t compute_hash(uint16_t parent, uint8_t arc_label) 
+{
+	return ((((parent << 8) | arc_label)+1) % DICTIONARY_SIZE);
+}
+
+void insert_in_collision_list(struct hash_entry* hash_entry, uint16_t parent)  //collision occurs
+{
+	while(hash_entry -> next != NULL)
+		hash_entry = hash_entry -> next;
+
+	hash_entry -> next = (struct hash_entry*) calloc(1,sizeof(struct hash_entry));
+
+	hash_entry -> next -> parent = parent;
+	hash_entry -> next -> index = node_id;
+	hash_entry -> next -> next = NULL;
+
+	node_id++;
+}
+
+void hash_insertion(uint16_t position, uint16_t parent, uint16_t node_id)
+{
+		//printf("%"PRIu16"\n",node_id);
+		hash_table[position] -> parent = parent;
+		hash_table[position] -> index = node_id;
+}
+
+void generate_root_childs() 
+{
+	uint8_t arc_label = 0x00;
+	uint16_t position;
+	node_id = 1;					//node_id = 0 = ROOT_ID 				
+	
+	hash_insertion(0,0,0);			//insert the ROOT
+
+	while(arc_label <= 0xFF) 
+	{	
+		position = compute_hash(ROOT_ID,arc_label);
+
+		hash_insertion(position,ROOT_ID,node_id);
+		
+		node_id++;
+
+		if(node_id == 257)		
+			break;			//otherwise overflow arc_label 
+		else
+			arc_label++;
+	}
+
+	//printf("%"PRIu16"\n",node_id);
+	//printf("%"PRIu8"\n",arc_label);
+}
+
+void build_hash_table() 
+{
+	int i;
+
+	for(i=0; i < DICTIONARY_SIZE; i++)
+		hash_table[i] = (struct hash_entry*) calloc(1,sizeof(struct hash_entry));
+
+	hash_init();
+	
+}
+
+void print_hash_table() 
+{
+
+	int i;
+
+	for(i=0; i < DICTIONARY_SIZE; i++)
+	{
+		printf("\nENTRY:%d",i);
+		printf("\tPARENT:%d",hash_table[i]->parent);
+		printf("\t:NODE:%d",hash_table[i]->index);	
+	}
+	printf("\n");
+}
+
+int hash_is_not_full() 
+{
+	return (node_id <= DICTIONARY_SIZE -1) ? 0 : 1;
+}
+
+void collision_list_destroy() 
+{
+	int i;
+	struct hash_entry* temp;
+
+	for(i=0; i < DICTIONARY_SIZE; i++)
+	{
+		temp = hash_table[i];
+		hash_table[i] = hash_table[i] -> next;
+
+		while(hash_table[i] != NULL)
+		{
+			temp = hash_table[i];
+			hash_table[i] = hash_table[i] -> next;
+			free(temp);
+		}
+
+	}
+}	
+
+void rebuild_hash_table() 
+{
+	node_id = 0;
+	collision_list_destroy();
+	hash_init();
+	generate_root_childs();
+}
+
+uint16_t check_for_free_entry_and_add(uint16_t position, uint16_t parent, uint64_t insertion_place) 
+{
+	if(hash_is_not_full() == 0 && insertion_place == HASH_ENTRY)
+	{	
+		hash_insertion(position,parent,node_id);
+		node_id++;
+	}
+	else if(hash_is_not_full() == 0 && insertion_place == COLLISION_LIST)
+		insert_in_collision_list(hash_table[position],parent);
+	else
+		rebuild_hash_table();
+
+	return parent;
+}
+
+
+
+uint16_t lookup_collision_list(uint16_t position, uint16_t parent) 
+{	
+	uint16_t child_id = 0;
+	struct hash_entry * hash_entry = hash_table[position];
+
+	while(hash_entry -> next != NULL)
+	{
+		if(hash_entry -> parent == parent) 
+		{
+			child_id = hash_entry -> index;
+			break;
+		}
+
+		hash_entry = hash_entry -> next;
+	}
+
+	if(child_id == 0)
+		return check_for_free_entry_and_add(position, parent, COLLISION_LIST);		//insert new symbhol in the collision list of entry pointed by position	
+														
+	return child_id;
+}
+
+
+
+
+uint16_t hash_lookup(uint16_t parent, uint8_t arc_label)
+{
+	uint16_t position = compute_hash(parent,arc_label);
+
+	if(hash_table[position] -> index != 0)		
+	{
+		if(hash_table[position] -> parent == parent)
+			return hash_table[position] -> index;
+
+		return lookup_collision_list(position,parent);
+	}
+
+	return check_for_free_entry_and_add(position,parent,HASH_ENTRY);
+}
+
+void hash_table_destroy()
+{	
+	int i;
+	
+	collision_list_destroy();
+
+	for(i=0; i < DICTIONARY_SIZE; i++){
+		free(hash_table[i]);
+	}
+}
+
+
+int lz78_compressor(const char* inputfilename, const char* outputfilename) {
+
+	struct bit_io* bitio_inputfile;
+	struct bit_io* bitio_outputfile;
+	uint64_t buff = 0;
+	uint8_t arc_label = 0;
+
+	build_hash_table();
+	generate_root_childs();
+
+	//print_hash_table();
+	
+	bitio_inputfile = bitio_open(inputfilename,READ);
+	if(bitio_inputfile == NULL)
+		return BITIO_OPEN_ERROR;
+
+	bitio_outputfile = bitio_open(outputfilename,WRITE);
+	if(bitio_outputfile == NULL)
+		return BITIO_OPEN_ERROR;
+
+	parent_node = ROOT_ID;
+	previous_parent_node = ROOT_ID;
+
+	while(bitio_read(bitio_inputfile,8,&buff) != -1) 
+	{			
+				printf("Letto:");
+				printf("%"PRIu64"\n",buff);
+				arc_label = (uint8_t)buff; 
+
+				parent_node = hash_lookup(parent_node, arc_label);
+		CHECK:
+				if(parent_node != previous_parent_node)										//child has been found
+					previous_parent_node = parent_node;
+				else																		//	new child has been added so we emit
+				{	
+					printf("Scritto:");
+					printf("%"PRIu64"\n",(uint64_t)parent_node);	
+					bitio_write(bitio_outputfile,16,(uint64_t)parent_node);
+					parent_node = ROOT_ID;
+					previous_parent_node = ROOT_ID;
+					printf("Letto char:");
+					printf("%"PRIu8"\n",arc_label);
+					parent_node = hash_lookup(parent_node, arc_label);
+					goto CHECK;
+				}	 
+	}
+
+	const uint64_t END_OF_FILE = 0;
+	bitio_write(bitio_outputfile,16, END_OF_FILE);
+	printf("%"PRIu64"\n",END_OF_FILE);
+
+	if(bitio_close(bitio_inputfile) == -1)
+		return BITIO_CLOSE_ERROR;
+
+	if(bitio_close(bitio_outputfile) == -1)
+		return BITIO_CLOSE_ERROR;
+
+	hash_table_destroy();
+
+	return 0;
+}
+
+/*
+	======================================================================================================
+												DECOMPRESSOR
+	======================================================================================================
+*/
+
+/*the dictionary is an array of decompresso_entry implementing the encoding tree*/
+struct decompressor_entry
+{
+	uint16_t father;
+	uint8_t symbol;
+};
+
+
+
+void init_decompressor_dictionary(struct decompressor_entry * dictionary)
+{
+	int i;
+	/*initializing the dictionary with the alphabet: the 256 ASCII symbols*/
+	dictionary[0].symbol = ROOT;												/* Node 0 --> ROOT & end the decoding*/
+	dictionary[0].father = ROOT;
+	
+	for(i=1; i<257; i++)
+	{		
+			dictionary[i].symbol = i-1;
+			dictionary[i].father = ROOT;
+	}
+
+	counter=257;
+}
+
+
+
+struct decompressor_entry * create_decompressor_dictionary()
+{
+	struct decompressor_entry *dictionary;
+	
+	dictionary = (struct decompressor_entry *)calloc(DICTIONARY_SIZE, sizeof(struct decompressor_entry));
+	if(dictionary == NULL) 
+	{
+		errno = ENOMEM;
+		return NULL; 
+	}
+	
+	init_decompressor_dictionary(dictionary);
+	
+	return dictionary;
+}
+
+
+
+int emit_decoding(struct bit_io* output, struct decompressor_entry* dictionary, uint16_t node, char *decode_buffer){
+
+	int i, ret;
+	int length=0;
+	uint64_t buffer;
+	
+	struct decompressor_entry* app;
+	app = &dictionary[node];
+			
+	do{
+		decode_buffer[length++] = app->symbol;
+		app = &dictionary[app->father];
+	}		
+	while(app->symbol != ROOT);
+	
+	for(i=length-1; i>=0; i--)
+	{
+		buffer = (uint64_t)decode_buffer[i];
+
+		ret = bitio_write(output, 8, buffer);
+		if(ret != 0)
+		{
+			printf( "Error bitio_write: %s\n", strerror( errno ) );
+			return -1;
+		}
+
+		printf( "\nLunghezza: %d - Scrivo: %c\n", i+1, (char)buffer );												/*_______________________controllo scrittura decodifica*/
+
+	}
+	
+	return 0;
+}
+
+
+
+uint16_t get_root_child_symbol(struct decompressor_entry* dictionary, uint16_t node)
+{
+	struct decompressor_entry* app;
+	app = &dictionary[node];
+	
+	while(app->father != ROOT)
+		app = &dictionary[app->father];
+		
+	return app->symbol;
+}
+
+
+
+int lz78_decompressor(const char* inputfilename, const char* outputfilename) 
+{
+
+
+	int ret;
+	uint16_t previous_node;			/*the previous dictionary entry read from the compressed file*/
+	uint16_t current_node;			/*the current dictionary entry read from the compressed file*/
+	uint64_t buffer;				/*to store the bitio_read data*/
+	
+	char * decode_buffer;			/*buffer to decode the symbols encoded in the dictionary entry*/
+	
+	decode_buffer = (char *)calloc(DICTIONARY_SIZE, sizeof(*decode_buffer));
+
+	if(decode_buffer == NULL) 
+	{
+		errno = ENOMEM;
+		printf( "Error allocating decoding buffer: %s\n", strerror( errno ) );
+		return -1; 
+	}
+	
+	/*dictionary creation*/
+	struct decompressor_entry *dictionary;
+		
+	dictionary = create_decompressor_dictionary();
+	if(dictionary == NULL)
+	{
+		printf( "Error creating the dictionary: %s\n", strerror( errno ) );
+		return -1;	
+	}
+	
+	/*files opening*/
+	struct bit_io *input;
+	struct bit_io *output;
+	
+	input = bitio_open(inputfilename, READ);
+	if(input == NULL)
+	{
+		printf( "Error opening %s: %s\n", inputfilename, strerror( errno ) );
+		return -1;
+	}
+	
+	output = bitio_open(outputfilename, WRITE);
+	if(output == NULL)
+	{
+		printf( "Error opening %s: %s\n", outputfilename, strerror( errno ) );
+		return -1;
+	}
+	
+	/*handling first dictionary access*/
+	ret = bitio_read(input,16, &buffer);
+	printf("\nLeggo BUFFER: %"PRIu64, buffer);
+	if(ret < 0)
+	{
+		printf( "Error bitio_read: %s\n", strerror( errno ) );
+		return -1;
+	}
+	current_node = (uint16_t)buffer;
+	printf("\nCURRENT NODE: %"PRIu16, current_node);
+
+	emit_decoding(output, dictionary, current_node, decode_buffer);
+	
+	previous_node = current_node;
+	
+	ret = bitio_read(input, 16, &buffer);
+	if(ret < 0)
+	{
+		printf( "Error bitio_read: %s\n", strerror( errno ) );
+		return -1;
+	}
+	current_node = (uint16_t)buffer;
+
+	/*from the second index in the compressed file*/
+	while(current_node != ROOT)																/*TESTARE CONDIZIONE DI USCITA! ROOT == EOF*/
+	{
+		/*adding new node (entry in the dictionary) to the previous node*/
+		dictionary[counter].father = previous_node;
+		dictionary[counter].symbol = get_root_child_symbol(dictionary, current_node);
+		
+		/*emitting the decoding*/
+		ret = emit_decoding(output, dictionary, current_node, decode_buffer);
+		if(ret != 0)
+		{
+			printf( "Error decoding : %s\n", strerror( errno ) );
+			return -1;
+		}
+		
+		counter++;
+		/*checking if dictionary is full*/
+		if(counter >= DICTIONARY_SIZE)
+		{
+			printf("passo");
+			init_decompressor_dictionary(dictionary);
+						
+			/*handling first dictionary access*/
+			ret = bitio_read(input, 16, &buffer);
+			if(ret < 0)
+			{
+				printf( "Error bitio_read: %s\n", strerror( errno ) );
+				return -1;
+			}
+			
+			current_node = (uint16_t)buffer;
+			
+			emit_decoding(output, dictionary, current_node, decode_buffer);
+		}
+		previous_node = current_node;
+		printf("\nCURRENT NODE: %"PRIu16, current_node);
+		/*reading next node*/
+		buffer = 0;
+		ret = bitio_read(input, 16, &buffer);
+		printf("\nLeggo BUFFER: %"PRIu64, buffer);
+		if(ret < 0)
+		{
+			printf( "\nError bitio_read: %s\n", strerror( errno ) );
+			return -1;
+		}
+		current_node = (uint16_t)buffer;	
+		
+/* ______________________________________________________________________________________________________________________________________test*/	
+		printf("\nLeggo: %"PRIu16, current_node);
+
+		if(current_node == ROOT)
+		{	
+			int i;
+			printf("\nPrinting the Dictionary\n");
+
+			for(i=257; i<counter; i++)
+			printf("\n%d)\t%c\tfather:%d\n", i, dictionary[i].symbol, dictionary[i].father);
+			break;
+		}
+/* ____________________________________________________________________________________________________________________________________fine test*/	
+	}
+
+	/*closing bit_io structures*/
+	ret = bitio_close(input);
+	if(ret != 0)
+	{
+		printf( "Error closing %s: %s\n", inputfilename, strerror( errno ) );
+		return -1;
+	}
+	ret = bitio_close(output);
+	if(ret != 0)
+	{
+		printf( "Error closing %s: %s\n", outputfilename, strerror( errno ) );
+		return -1;
+	}
+	
+	/*deallocating memory*/	
+	bzero(decode_buffer, DICTIONARY_SIZE*sizeof(*decode_buffer));
+	free(decode_buffer);
+	
+	bzero(dictionary, DICTIONARY_SIZE*sizeof(*dictionary));
+	free(dictionary);
+	
+	return 0;
+}
